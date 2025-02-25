83b81edb103ef8621c98a75d4d89bb3cc2beb052
Equiel-1703 <henriquegabriel22.hg@gmail.com>
2025-02-25 00:37:08 -0300

    Co-authored-by: Gabriel_Rosa <GabrielRosaO@users.noreply.github.com> Co-authored-by: Daniel Lisboa <danlisb@users.noreply.github.com>

---
+++ server.py
@@ -0,0 +1,2 @@
+# Código que gerencia o servidor e a conexão com o cliente
+# Autores: Henrique Rodrigues, Daniel Lisboa & Gabriel Pink
---
+++ start_server.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+CONFIG_FILE="/etc/samba/smb.conf"
+NOME_DO_COMPARTILHAMENTO="PS2SMB"
+PATH_DA_PASTA="/home/$SUDO_USER/$NOME_DO_COMPARTILHAMENTO"
+
+# Cria a pasta compartilhada com o nome "PS2SMB"
+create_shared_folder()
+{
+    echo "Criando pasta compartilhada em $PATH_DA_PASTA"
+    mkdir -p $PATH_DA_PASTA
+
+    # Torna a pasta compartilhada acessível para todos os usuários
+    chmod 777 $PATH_DA_PASTA
+}
+
+# Verifica se o samba está instalado
+check_samba()
+{
+    if dpkg -s samba &>/dev/null; then
+        echo 'O samba está instalado corretamente.'
+    else
+        echo 'Parece que você não tem o samba instalado. Por favor, instale o samba e tente novamente.'
+        exit
+    fi
+}
+
+# Confere se o compartilhamento PS2SMB está público, para permitir o acesso ao PS2
+check_smb_server_exists()
+{
+    read_next_line=false
+
+    regex_1="guest ok.*"
+    regex_2=".*yes"
+
+    cat $CONFIG_FILE | while IFS= read -r line; do
+        if [[ $read_next_line == true ]]; then
+            if [[ $line =~ $regex_1 ]]; then
+                if [[ $line =~ $regex_2 ]]; then
+                    echo "O compartilhamento existe e o acesso público está habilitado. OK!"
+                    return 1
+                else
+                    echo "O compartilhamento existe, mas o acesso público não está habilitado. Por favor, edite as configurações no seu arquivo smb.conf para habilitar o acesso público (guest ok = yes) no compartilhamento $NOME_DO_COMPARTILHAMENTO."
+                    return -1
+                fi
+            fi
+        fi
+
+        if [[ "$line" == "[$NOME_DO_COMPARTILHAMENTO]" ]]; then
+            read_next_line=true
+
+            echo "O compartilhamento $NOME_DO_COMPARTILHAMENTO foi encontrado no arquivo de configuração do samba."
+
+            continue
+        fi
+    done
+
+    return 0
+}
+
+create_smb_config()
+{
+    echo "[$NOME_DO_COMPARTILHAMENTO]" >> $CONFIG_FILE
+    echo "  path = $PATH_DA_PASTA" >> $CONFIG_FILE
+    echo "  guest ok = yes" >> $CONFIG_FILE
+    echo "  read only = no" >> $CONFIG_FILE
+}
+
+# Verifica se o script está usando o comando sudo (usuário root)
+if [[ "$EUID" -ne 0 ]]; then
+    echo "Por favor, execute o script como root."
+    exit
+fi
+
+# Verifica se o samba está instalado
+check_samba
+
+# Verifica se a pasta de compartilhamento existe
+if [[ -d $PATH_DA_PASTA ]]; then
+    echo "A pasta de compartilhamento $PATH_DA_PASTA já existe."
+else
+    create_shared_folder
+fi
+
+check_smb_server_exists
+ret=$?
+
+if [[ $ret -eq -1 ]]; then
+    exit 1
+elif [[ $ret -eq 1 ]]; then
+    echo "O compartilhamento $NOME_DO_COMPARTILHAMENTO não foi encontrado no arquivo de configuração do samba. Criando o compartilhamento $NOME_DO_COMPARTILHAMENTO ao arquivo de configuração do samba.."
+    create_smb_config
+    echo "Compartilhamento $NOME_DO_COMPARTILHAMENTO adicionado ao arquivo de configuração do samba $CONFIG_FILE."
+fi
