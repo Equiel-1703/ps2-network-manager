683f2e22242401a0a1ac6d8ef1bde571ab321679
Equiel-1703 <henriquegabriel22.hg@gmail.com>
2025-02-25 01:44:48 -0300

    Shell script done

--- start_server.sh
+++ start_server.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
+# Configurações do samba
 CONFIG_FILE="/etc/samba/smb.conf"
 NOME_DO_COMPARTILHAMENTO="PS2SMB"
 PATH_DA_PASTA="/home/$SUDO_USER/$NOME_DO_COMPARTILHAMENTO"
+PYTHON_SCRIPT="server_manager.py"
+
+# Códigos de cores
+RED='\033[0;31m'     # Vermelho
+GREEN='\033[0;32m'   # Verde
+YELLOW='\033[1;33m'  # Amarelo
+NC='\033[0m'        # Sem cor
 
 # Cria a pasta compartilhada com o nome "PS2SMB"
 create_shared_folder()
 {
     echo "Criando pasta compartilhada em $PATH_DA_PASTA"
-    mkdir -p $PATH_DA_PASTA
+    sudo -c mkdir -p $PATH_DA_PASTA
 
     # Torna a pasta compartilhada acessível para todos os usuários
-    chmod 777 $PATH_DA_PASTA
+    sudo chmod 777 $PATH_DA_PASTA
 }
 
 # Verifica se o samba está instalado
 check_samba()
 {
     if dpkg -s samba &>/dev/null; then
-        echo 'O samba está instalado corretamente.'
+        echo -e "${GREEN}O samba está instalado corretamente.${NC}"
     else
-        echo 'Parece que você não tem o samba instalado. Por favor, instale o samba e tente novamente.'
+        echo -e "${RED}Parece que você não tem o samba instalado. Por favor, instale o samba e tente novamente.${NC}"
         exit
     fi
 }
@@ -28,20 +36,23 @@ check_samba()
 # Confere se o compartilhamento PS2SMB está público, para permitir o acesso ao PS2
 check_smb_server_exists()
 {
+    retorno=1
     read_next_line=false
 
     regex_1="guest ok.*"
     regex_2=".*yes"
 
-    cat $CONFIG_FILE | while IFS= read -r line; do
+    while IFS= read -r line; do
         if [[ $read_next_line == true ]]; then
             if [[ $line =~ $regex_1 ]]; then
                 if [[ $line =~ $regex_2 ]]; then
-                    echo "O compartilhamento existe e o acesso público está habilitado. OK!"
-                    return 1
+                    echo -e "${GREEN}O compartilhamento existe e o acesso público está habilitado. OK!${NC}"
+                    retorno=2
+                    break
                 else
-                    echo "O compartilhamento existe, mas o acesso público não está habilitado. Por favor, edite as configurações no seu arquivo smb.conf para habilitar o acesso público (guest ok = yes) no compartilhamento $NOME_DO_COMPARTILHAMENTO."
-                    return -1
+                    echo -e "${RED}O compartilhamento existe, mas o acesso público não está habilitado. Por favor, edite as configurações no seu arquivo smb.conf para habilitar o acesso público (guest ok = yes) no compartilhamento $NOME_DO_COMPARTILHAMENTO.${NC}"
+                    retorno=0
+                    break
                 fi
             fi
         fi
@@ -49,41 +60,43 @@ check_smb_server_exists()
         if [[ "$line" == "[$NOME_DO_COMPARTILHAMENTO]" ]]; then
             read_next_line=true
 
-            echo "O compartilhamento $NOME_DO_COMPARTILHAMENTO foi encontrado no arquivo de configuração do samba."
+            echo -e "${GREEN}O compartilhamento $NOME_DO_COMPARTILHAMENTO foi encontrado no arquivo de configuração do samba.${NC}"
 
             continue
         fi
-    done
+    done < "$CONFIG_FILE"
 
-    return 0
+    return $retorno
 }
 
 # Cria o arquivo de configuração do samba
 create_smb_config()
 {
-    echo "" >> $CONFIG_FILE
-    echo "[$NOME_DO_COMPARTILHAMENTO]" >> $CONFIG_FILE
-    echo "  path = $PATH_DA_PASTA" >> $CONFIG_FILE
-    echo "  guest ok = yes" >> $CONFIG_FILE
-    echo "  read only = no" >> $CONFIG_FILE
-    echo "  writeable = yes" >> $CONFIG_FILE
-    echo "  browsable = yes" >> $CONFIG_FILE
-    echo "  create mask = 0777" >> $CONFIG_FILE
-    echo "  directory mask = 0777" >> $CONFIG_FILE
+    sudo bash -c "cat <<EOL >> $CONFIG_FILE
+
+[$NOME_DO_COMPARTILHAMENTO]
+  path = $PATH_DA_PASTA
+  guest ok = yes
+  read only = no
+  writeable = yes
+  browsable = yes
+  create mask = 0777
+  directory mask = 0777
+EOL"
 }
 
 # Verifica se o script está usando o comando sudo (usuário root)
-if [[ "$EUID" -ne 0 ]]; then
-    echo "Por favor, execute o script como root."
-    exit
-fi
+# if [[ "$EUID" -ne 0 ]]; then
+#     echo -e "${RED}Por favor, execute o script como root.${NC}"
+#     exit
+# fi
 
 # Verifica se o samba está instalado
 check_samba
 
 # Verifica se a pasta de compartilhamento existe
 if [[ -d $PATH_DA_PASTA ]]; then
-    echo "A pasta de compartilhamento $PATH_DA_PASTA já existe."
+    echo -e "${YELLOW}A pasta de compartilhamento $PATH_DA_PASTA já existe.${NC}"
 else
     create_shared_folder
 fi
@@ -92,10 +105,13 @@ fi
 check_smb_server_exists
 ret=$?
 
-if [[ $ret -eq -1 ]]; then
+if [[ $ret -eq 0 ]]; then
     exit 1
-elif [[ $ret -eq 0 ]]; then
-    echo "O compartilhamento $NOME_DO_COMPARTILHAMENTO não foi encontrado no arquivo de configuração do samba. Criando o compartilhamento $NOME_DO_COMPARTILHAMENTO.."
+elif [[ $ret -eq 1 ]]; then
+    echo -e "${YELLOW}O compartilhamento $NOME_DO_COMPARTILHAMENTO não foi encontrado no arquivo de configuração do samba. Criando o compartilhamento $NOME_DO_COMPARTILHAMENTO...${NC}"
     create_smb_config
-    echo "Compartilhamento $NOME_DO_COMPARTILHAMENTO adicionado ao arquivo de configuração do samba $CONFIG_FILE"
+    echo -e "${GREEN}Compartilhamento $NOME_DO_COMPARTILHAMENTO adicionado ao arquivo de configuração do samba $CONFIG_FILE ${NC}"
 fi
+
+echo -e "${GREEN}Iniciando script de gerenciamento em python..${NC}"
+python $PYTHON_SCRIPT
