f14eadda0776051848e9edab31aa813979473d78
Equiel-1703 <henriquegabriel22.hg@gmail.com>
2025-03-11 23:29:50 -0300

    Improved GUI

--- modules/Exceptions.py
+++ modules/Exceptions.py
@@ -4,10 +4,22 @@ class BaseManagerException(Exception):
     def __init__(self, error_message, description=""):
         self.error_message = error_message
         self.description = description
-        super().__init__(f"{Fore.RED}Error: {error_message}\n\n{description}")
+        super().__init__(f"{Fore.RED}ERRO: {error_message}\n{description}")
+
+class SambaConfNotFound(BaseManagerException):
+    def __init__(self, samba_conf_path):
+        self.error_message = "Arquivo de configuração do SAMBA não encontrado."
+        self.description = f"Por favor, verifique se o arquivo de configuração do SAMBA em '{samba_conf_path}' existe e tente novamente."
+        super().__init__(self.error_message, self.description)
 
 class GlobalSettingsNotFound(BaseManagerException):
     def __init__(self):
         self.error_message = "Seção [global] não encontrada no arquivo de configuração do SAMBA."
-        self.description = f"Por favor, verifique se o arquivo de configuração do SAMBA {self.SAMBA_CONF_PATH} está correto e tente novamente."
+        self.description = f"Por favor, verifique se o arquivo de configuração do SAMBA está correto e tente novamente."
+        super().__init__(self.error_message, self.description)
+
+class SettingNotFound(BaseManagerException):
+    def __init__(self, setting_name):
+        self.error_message = f"Configuração '{setting_name}' não encontrada no arquivo de configuração do SAMBA."
+        self.description = f"Por favor, verifique se o arquivo de configuração do SAMBA está correto e tente novamente."
         super().__init__(self.error_message, self.description)
--- modules/GUI.py
+++ modules/GUI.py
@@ -1,46 +1,153 @@
-import sys
-from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout
+from PyQt6.QtWidgets import *
 from PyQt6.QtGui import QFontDatabase, QFont
-from PyQt6.QtCore import Qt
+from PyQt6.QtCore import Qt, QRect
 
-class PS2NetManager(QWidget):
+# Importing our custom modules
+from modules.SambaManager import SambaManager
+
+class PS2NetManagerGUI(QMainWindow):
     # Dark Purple Palette
-    _COLORS = {
+    __COLORS = {
         "bg": "#1A1326",  # Deep Purple
         "frame_bg": "#33294E",  # Dark Lavender
         "button_bg": "#815AC0",  # Soft Violet
         "text_color": "#EDE7F6",  # Off-White Text
     }
 
+    __WINDOW_DIMENSIONS = (800, 600)
+
+    # Defining fonts
+    __LIGHT_FONT = QFont("Quicksand Light", 16)
+    __REGULAR_FONT = QFont("Quicksand Regular", 14)
+    __BOLD_FONT = QFont("Quicksand Bold", 16, QFont.Weight.Bold)
+
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("PS2 Network Manager")
-        self.setGeometry(100, 100, 600, 600)
 
-        # Carrega fontes personalizadas
-        self._load_custom_fonts()
+        # Center window on screen
+        window_rec = QRect(0,0, self.__WINDOW_DIMENSIONS[0], self.__WINDOW_DIMENSIONS[1])
+        window_rec.moveCenter(QApplication.primaryScreen().geometry().center())
 
-        menu_font = QFont("Quicksand Light", 16)
+        self.setGeometry(window_rec)
+        self.setFixedSize(self.__WINDOW_DIMENSIONS[0], self.__WINDOW_DIMENSIONS[1])
 
-        # Create label with custom font
-        label = QLabel("Hello, PyQt with Custom Font!", self)
-        label.setFont(menu_font)
+        self.__load_custom_fonts()
 
-        # Layout
-        layout = QVBoxLayout()
-        layout.addWidget(label)
-        layout.setAlignment(Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignHCenter)
-        self.setLayout(layout)
+        # Layouts
+        ML_MARGIN = 10
+        ML_SPACING = 15
 
-    def _load_custom_fonts(self):
+        main_layout = QVBoxLayout()
+        main_layout.setContentsMargins(ML_MARGIN, ML_MARGIN, ML_MARGIN, ML_MARGIN)
+        main_layout.setSpacing(ML_SPACING)
+
+        # Creating layouts for the sections of the GUI
+        netbios_widget = self.__create_netbios_widget()
+        share_name_widget = self.__create_share_name_widget()
+        shared_folder_widget = self.__create_shared_folder_path_widget()
+
+        # Adding the widgets to the main layout
+        main_layout.addWidget(netbios_widget)
+        main_layout.addWidget(share_name_widget)
+        main_layout.addWidget(shared_folder_widget)
+
+        main_layout.addStretch()
+
+        # Creating the main widget
+        main_widget = QWidget()
+        main_widget.setLayout(main_layout)
+        main_widget.setStyleSheet(f"background-color: {self.__COLORS['bg']};")
+
+        self.setCentralWidget(main_widget)
+
+    def __load_custom_fonts(self):
         QFontDatabase.addApplicationFont("fonts/Quicksand-Light.ttf")
         QFontDatabase.addApplicationFont("fonts/Quicksand-Regular.ttf")
         QFontDatabase.addApplicationFont("fonts/Quicksand-Bold.ttf")
 
-if __name__ == "__main__":
-    # Executa a aplicação
-    app = QApplication([])
-    window = PS2NetManager()
-    window.show()
-    sys.exit(app.exec())
+    def __create_netbios_widget(self):
+        netbios_layout = QHBoxLayout()
+
+        label = QLabel("NOME NETBIOS:", self)
+        label.setFont(self.__LIGHT_FONT)
+
+        line_field = QLineEdit(self)
+        line_field.setPlaceholderText("Nome NetBIOS")
+        line_field.setFont(self.__REGULAR_FONT)
+        line_field.setStyleSheet(f"background-color: {self.__COLORS['frame_bg']}; color: {self.__COLORS['text_color']};")
+        line_field.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed)
+
+        ok_button = QPushButton("OK", self)
+        ok_button.setFont(self.__BOLD_FONT)
+        ok_button.setStyleSheet(f"background-color: {self.__COLORS['button_bg']}; color: {self.__COLORS['text_color']};")
+        ok_button.clicked.connect(lambda: print("OK button clicked"))
+
+        LINE_FIELD_H_SPACE = 10
+
+        netbios_layout.addWidget(label)
+        netbios_layout.addSpacerItem(QSpacerItem(LINE_FIELD_H_SPACE, 0))
+        netbios_layout.addWidget(line_field)
+        netbios_layout.addSpacerItem(QSpacerItem(LINE_FIELD_H_SPACE, 0))
+        netbios_layout.addWidget(ok_button)
+
+        netbios_layout.setContentsMargins(0, 0, 0, 0)
+
+        widget = QWidget()
+        widget.setLayout(netbios_layout)
+
+        return widget
+
+    def __create_share_name_widget(self):
+        share_name_layout = QHBoxLayout()
+
+        label = QLabel("NOME DO COMPARTILHAMENTO:", self)
+        label.setFont(self.__LIGHT_FONT)
+
+        share_name_label = QLabel("PS2SMB", self)
+        share_name_label.setFont(self.__BOLD_FONT)
+        share_name_label.setStyleSheet(f"color: {self.__COLORS['text_color']};")
+        
+        share_name_label.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed)
+        share_name_label.setAlignment(Qt.AlignmentFlag.AlignRight)
+        
+        share_name_layout.addWidget(label)
+        share_name_layout.addWidget(share_name_label)
+
+        share_name_layout.setContentsMargins(0, 0, 0, 0)
+
+        widget = QWidget()
+        widget.setLayout(share_name_layout)
+
+        return widget
+    
+    def __create_shared_folder_path_widget(self):
+        shared_folder_layout = QHBoxLayout()
+
+        label = QLabel("PASTA COMPARTILHADA:", self)
+        label.setFont(self.__LIGHT_FONT)
+
+        shared_folder_path = QLabel("/home/user/ps2smb", self)
+        shared_folder_path.setFont(self.__BOLD_FONT)
+        shared_folder_path.setStyleSheet(f"color: {self.__COLORS['text_color']};")
+        shared_folder_path.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed)
+        shared_folder_path.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        change_folder_button = QPushButton("ALTERAR", self)
+        change_folder_button.setFont(self.__BOLD_FONT)
+        change_folder_button.setStyleSheet(f"background-color: {self.__COLORS['button_bg']}; color: {self.__COLORS['text_color']};")
+        change_folder_button.clicked.connect(lambda: print("Change folder button clicked"))
+
+        shared_folder_layout.addWidget(label)
+        shared_folder_layout.addWidget(shared_folder_path)
+        shared_folder_layout.addWidget(change_folder_button)
+
+        shared_folder_layout.setContentsMargins(0, 0, 0, 0)
+
+        widget = QWidget()
+        widget.setLayout(shared_folder_layout)
+        return widget
+    
+    def __ip_settings_widget(self):
+        pass
